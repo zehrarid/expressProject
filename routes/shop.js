51a7df511dbb4061d84f04df2f0887d706bcb718
Zehra Rıdvanoğulları <91746347+zehrarid@users.noreply.github.com>
2024-04-16 18:09:26 +0300

    first commit

---
+++ routes/shop.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+
+const adminData = require("./admin");
+
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+});
+
+module.exports = router;
