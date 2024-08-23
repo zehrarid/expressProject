9780c5e9493f77a4ee52b7f5d19b193d6c5a331e
Zehra Rıdvanoğulları <91746347+zehrarid@users.noreply.github.com>
2024-08-23 19:26:13 +0300

    commit

--- routes/admin.js
+++ routes/admin.js
@@ -3,26 +3,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const rootDir = require("../util/path");
-
-const products = [];
+const productsController = require("../controllers/products");
 
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    activeAddProduct: true,
-    formsCSS: true,
-    productCSS: true,
-  });
-});
+router.get("/add-product", productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+router.post("/add-product", productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
